b3c2dafcb3266fe85562e40c289f0b6546daf784
serxan22 <82658453+serxan22@users.noreply.github.com>
2021-05-19 17:36:35 +0400

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -42,8 +42,8 @@ start = time()
 delta_ping = time() - start
 
 PING_MSG = f"""🏋🏻 ping🤸🏻‍♀️: `{delta_ping * 1000:.3f} ms`
-✯provided by 🤖 **[TamilBots](https://t.me/tamilbots)**
-✯For Support 🆘 **[TamilSupport](https://t.me/TamilSupport)**"""
+☬ **[DrakulaBots](https://t.me/drakulabots)** Tərəfindən hazırlanıb.🤖🔧
+☬ **[DarkLegionChat](https://t.me/darklegionchat)** Chat qrupumuz✅"""
 
 @Client.on_message(filters.text
                    & self_or_contact_filter
